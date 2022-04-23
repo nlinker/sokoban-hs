b468f013d149a29aae18ebd3009841bb5a49b085
Nick Linker <nlinker@gmail.com>
2022-04-24 02:52:00 +0600

    sokoban: refactor algorithm to be in ST, wip

--- src/Sokoban/Solver.hs
+++ src/Sokoban/Solver.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
@@ -14,6 +15,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Sokoban.Solver where
 
@@ -21,17 +23,18 @@ import Prelude hiding (Left, Right, id)
 
 import Control.Lens            (use, (%=), (.=), (^.))
 import Control.Lens.TH         (makeLenses)
-import Control.Monad           (forM_, when)
+import Control.Monad           (forM_, when, filterM)
 import Control.Monad.State     (StateT, evalStateT, lift, gets)
 import Data.Hashable           (Hashable)
-import Control.Monad.Primitive (PrimMonad, PrimState)
+import Control.Monad.Primitive (PrimMonad(..), PrimState)
 import Control.Monad.ST        (ST, runST)
+import Control.Monad.ST.Trans  (runSTT, STT(..))
 
 import qualified Data.HashMap.Strict      as H
 import qualified Data.HashPSQ             as Q
-import qualified Data.Heap.Mutable.ModelD as HeapD
 import qualified Data.Map.Strict          as Map
 import qualified Data.Set                 as Set
+import qualified Data.Heap.Mutable.ModelD as HPM
 import qualified Data.HashMap.Mutable.Basic as HM
 
 import Data.Monoid   (All (..))
@@ -43,6 +46,8 @@ import Data.Function (on)
 import Control.Monad.Identity (runIdentity)
 import Debug.Trace (traceShowM, traceM, trace)
 import Text.InterpolatedString.QM (qm)
+import Control.Monad.ST.Trans.Internal (liftST, STT(..), STTRet(..))
+import Data.Maybe (fromMaybe, isJust)
 
 data Weight p =
   Weight
@@ -58,7 +63,7 @@ newtype Min =
 
 data AStar s p =
   AStar
-    { _heap :: HeapD.Heap s Min
+    { _heap :: HPM.Heap s Min
     , _closedList :: HM.MHashMap s p p
 --    , _openList   :: Q.HashPSQ p Int (Weight p)
     }
@@ -94,6 +99,13 @@ instance Monoid Min where
 newtype MyElement = MyElement { getMyElement :: Int }
   deriving (Show,Read,Eq,Ord)
 
+instance (Monad m) => PrimMonad (STT s m) where
+  type PrimState (STT s m) = s
+  primitive f =
+    STT $ \s ->
+      case f s of
+        (# t, a #) -> return (STTRet t a)
+  {-# INLINE primitive #-}
 
 -- λ> :rr heapMatchesList $ (\(f,s) -> (Min f, MyElement s)) <$> [(1,4),(2,3),(3,2),(4,1)]
 heapMatchesList :: [(Min,MyElement)] -> Bool
@@ -103,51 +115,69 @@ heapMatchesList xs' = runIdentity $ do
   let ys = Map.fromListWith mappend xsSet
   let listRes = Map.toList $ Map.fromListWith Set.union $ map (\(e,p) -> (p,Set.singleton e)) (Map.toList ys)
   let heapRes = runST $ do
-        h <- HeapD.new 1000
-        HeapD.pushList xs h
-        ij0 <- HeapD.pop h
+        h <- HPM.new 1000
+        HPM.pushList xs h
+        ij0 <- HPM.pop h
         traceM $ "ij0=" <> show ij0
-        HeapD.push (Min 2) 4 h
-        ij1 <- HeapD.pop h
+        HPM.push (Min 2) 4 h
+        ij1 <- HPM.pop h
         traceM $ "ij1=" <> show ij1
-        ij2 <- HeapD.pop h
+        ij2 <- HPM.pop h
         traceM $ "ij2=" <> show ij2
-        ij3 <- HeapD.pop h
+        ij3 <- HPM.pop h
         traceM $ "ij3=" <> show ij3
-        ij4 <- HeapD.pop h
+        ij4 <- HPM.pop h
         traceM $ "ij4=" <> show ij4
-        ijs <- HeapD.popAll h
-        return $ ijs
+        HPM.popAll h
   let heapResSet = map (\pairs@((p,_) : _) -> (p,Set.fromList $ map snd pairs))
         $ groupBy (on (==) fst) heapRes
   return $ trace [qm| xs={xs}\nheapRes={heapRes}|] $ heapResSet == listRes
 
-aStarFind :: (Monad m, Hashable p, Ord p) => AStarSolver m p -> p -> p -> (p -> m Bool) -> m [p]
+aStarFind :: (Monad m, Hashable p, Ord p, Show p) => AStarSolver m p -> p -> p -> (p -> m Bool) -> m [p]
 aStarFind solver src dst stopCond = do
-  let path = []
-  path <- runST $ do
-    astar <- aStarInitST src (p2i solver)
-    undefined
+  let maxNodesCount = 1000000
+  path <- runSTT $ do
+    heap <- HPM.new maxNodesCount
+    openList <- HM.new
+    closedList <- HM.newSized maxNodesCount
+    HM.insert openList src src
+    HPM.unsafePush (mempty :: Min) (p2i solver src) heap
+
+    top' <- HPM.pop heap -- remove the minimum and return
+    case top' of
+        Nothing -> return []
+        Just (fScore, i) -> do
+          let p0 = i2p solver i
+          finished <- lift $ stopCond p0
+          if finished
+            then do
+              -- HM.insert p0 (weight0 ^. parent) closedList
+              parent' <- HM.lookup openList p0
+              let parent = fromMaybe (error [qm|{p0} is not found in openList|]) parent'
+              HM.insert closedList p0 parent
+              backtraceST closedList p0
+            else do
+              parent' <- HM.lookup openList p0
+              let parent = fromMaybe (error [qm|{p0} is not found in openList|]) parent'
+              HM.insert closedList p0 parent
+              neighbors <- lift $ neighbors solver p0
+              neighPoints <- filterM (\p -> not <$> member closedList p) neighbors 
+
+--              let closed p = do
+--                    mb <- liftST $ HM.lookup closedList p
+--                    return undefined
+
+--          neighPoints <- liftST $ filterM _ neighs
+--          let neighPoints = filter (not . (`H.member` closedList0)) neighbors
+--          -- `k` is the current node, `fs` is f-score
+              undefined
   return path
   where
-
-aStarInitST :: (PrimMonad m, Ord p) => p -> (p -> Int) -> m (AStar (PrimState m) p)
-aStarInitST src p2i = do
-  heap <- HeapD.new 1000000
-  HeapD.unsafePush (mempty :: Min) (p2i src) heap
-  closedList <- HM.newSized 1000000
-  return $ AStar { _heap = heap, _closedList = closedList }
---    f2p k = Point (k `div` n) (k `mod` n)
-
-aStarFindST :: (PrimMonad m, Hashable p, Ord p) => m [p]
-aStarFindST = undefined
-
---aStarInit :: (Hashable p, Ord p) => p -> AStar p
---aStarInit src =
---  let weight = Weight {_fScore = 0, _gScore = 0, _parent = src}
---      openList = Q.singleton src (weight ^. fScore) weight
---      closedList = H.empty :: H.HashMap p p
---   in AStar openList closedList
+    -- member :: forall m p . (PrimMonad m, Hashable p, Ord p, Show p) => p -> m Bool
+    member :: (Monad m, Hashable k, Eq k) => HM.MHashMap s k a -> k -> STT s m Bool
+    member hmap p = do
+      v' <- HM.lookup hmap p
+      return $ isJust v'
 
 --aStarFindRec :: (Monad m, Hashable p, Ord p) => AStarSolver m p -> p -> (p -> m Bool) -> StateT (AStar p) m [p]
 --aStarFindRec solver dst stopCond = do
@@ -183,6 +213,37 @@ aStarFindST = undefined
 --                -- the neighbour is new
 --               -> openList .= Q.insert np f1 w1 openList0
 --          aStarFindRec solver dst stopCond
+     
+      
+backtraceST :: forall m p . (PrimMonad m, Eq p, Hashable p) => HM.MHashMap (PrimState m) p p -> p -> m [p]
+backtraceST closedList dst = backtraceRec dst [dst]
+  where
+    -- we repeatedly lookup for the parent of the current node
+    backtraceRec :: (PrimMonad m, Eq p) => p -> [p] -> m [p]
+    backtraceRec current acc = do
+      parent' <- HM.lookup closedList current
+      case parent' of
+        Nothing -> return []
+        Just parent
+          | current == parent -> return acc
+        Just parent -> backtraceRec parent (parent : acc)
+
+aStarFindST :: (PrimMonad m, Hashable p, Ord p) => AStarSolver m p -> p -> StateT (AStar (PrimState m) p) m [p]
+aStarFindST solver dst = undefined
+
+aStarInitST :: (PrimMonad m, Ord p) => p -> (p -> Int) -> m (AStar (PrimState m) p)
+aStarInitST src p2i = do
+  heap <- HPM.new 1000000
+  HPM.unsafePush (mempty :: Min) (p2i src) heap
+  closedList <- HM.newSized 1000000
+  return $ AStar { _heap = heap, _closedList = closedList }
+
+--aStarInit :: (Hashable p, Ord p) => p -> AStar p
+--aStarInit src =
+--  let weight = Weight {_fScore = 0, _gScore = 0, _parent = src}
+--      openList = Q.singleton src (weight ^. fScore) weight
+--      closedList = H.empty :: H.HashMap p p
+--   in AStar openList closedList
 
 backtrace :: (Eq p, Hashable p) => p -> H.HashMap p p -> [p]
 backtrace dst closedList = backtraceRec dst [dst]
