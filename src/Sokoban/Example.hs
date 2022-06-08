ec492e77b66c660f593019c2ee95ac3289e02328
Nick Linker <nlinker@gmail.com>
2022-06-09 03:21:00 +0600

    sokoban: bring TChan to the example

--- src/Sokoban/Example.hs
+++ src/Sokoban/Example.hs
@@ -1,40 +1,89 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE QuasiQuotes      #-}
-{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NumericUnderscores  #-}
+{-# LANGUAGE QuasiQuotes         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module Sokoban.Example where
 
-import Control.Concurrent         (forkIO, threadDelay)
-import Control.Concurrent.STM     (TVar, atomically, newTVar, newTVarIO, readTVar, writeTVar)
-import Control.Lens               ((^.))
+import Prelude hiding (id)
+
+import Control.Concurrent         (ThreadId, forkIO, killThread, threadDelay)
+import Control.Concurrent.STM     (TChan, TVar, atomically, newTChanIO, newTVar, newTVarIO,
+                                   readTVar, writeTChan, writeTVar)
+import Control.Lens               ((%=), (%~), (&), (.~), (?~), (^.))
 import Control.Lens.TH            (makeLenses)
 import Text.InterpolatedString.QM (qms)
 
 import Control.Monad.State.Strict (MonadState, evalState, execState)
 import System.IO.Unsafe           (unsafePerformIO)
 
-import qualified Data.Text    as T
-import qualified Data.Text.IO as T
+import           Control.Monad (forM_)
+import           Data.IORef    (newIORef, readIORef, writeIORef)
+import qualified Data.Text     as T
+import qualified Data.Text.IO  as T
+import           Sokoban.Level (Direction(..))
 
 data GameState =
   GameState
-    { _xxx :: Int
-    , _yyy :: Int
-    , _txt :: T.Text
+    { _xxx      :: Int
+    , _yyy      :: Int
+    , _txt      :: T.Text
+    , _commands :: [Command]
+    , _thread   :: Maybe ThreadId
     }
   deriving (Eq, Ord, Show)
 
+data Command
+  = StartThread
+  | StopThread
+  | Progress
+  deriving (Eq, Ord, Show)
+
 data Action
-  = Mark Int
+  = Move Direction
   | Start
   | Stop
+  deriving (Eq, Ord, Show)
 
 makeLenses ''GameState
 
+--backgroundThread :: TVar (Maybe ThreadId)
+--{-# NOINLINE backgroundThread #-}
+--backgroundThread = unsafePerformIO $ newTVarIO Nothing
 example :: IO ()
-example = do
-  let gs0 = GameState 0 0 ""
-  render gs0
+example
+  -- background <- newTVarIO Nothing :: IO (Maybe ThreadId)
+ = do
+  (channel :: TChan Command) <- newTChanIO
+  let gs = GameState 0 0 "" [] Nothing
+  gsRef <- newIORef gs
+  render gs
+  let actions = [Move L, Move U, Start]
+  forM_ actions $ \a -> do
+    gs <- readIORef gsRef
+    let gs1 = step gs a
+    gs2 <-
+      case head $ gs1 ^. commands of
+        StartThread -> do
+          id <- forkIO $ backgroundThread channel
+          return $ gs & thread ?~ id
+        StopThread -> do
+          let Just id = gs ^. thread
+          killThread id
+          return gs
+    threadDelay 100_000 -- 100 milliseconds
+    writeIORef gsRef gs2
+    render gs2
+  where
+    backgroundThread :: TChan Command -> IO ()
+    backgroundThread chan = do
+      threadDelay 1000_000 -- 1 second
+      atomically $ writeTChan chan Progress
+      backgroundThread chan
+
+applyV :: TVar a -> (a -> a) -> IO ()
+applyV x fn = atomically $ readTVar x >>= writeTVar x . fn
 
 step :: GameState -> Action -> GameState
 step state action = (execState $ runStep action) state
@@ -42,22 +91,23 @@ step state action = (execState $ runStep action) state
 runStep :: MonadState GameState m => Action -> m ()
 runStep action =
   case action of
-    Mark a -> mark
-    Start  -> startState
-    Stop   -> stopState
+    Move d -> move d
+    Start  -> start
+    Stop   -> stop
 
-mark :: MonadState GameState m => m ()
-mark = undefined
+move :: MonadState GameState m => Direction -> m ()
+move d =
+  case d of
+    U -> yyy %= pred
+    D -> yyy %= succ
+    L -> xxx %= pred
+    R -> xxx %= succ
 
-startState :: MonadState GameState m => m ()
-startState = undefined
+start :: MonadState GameState m => m ()
+start = undefined
 
-stopState :: MonadState GameState m => m ()
-stopState = undefined
-
-shared :: TVar Integer
-{-# NOINLINE shared #-}
-shared = unsafePerformIO $ newTVarIO 0
+stop :: MonadState GameState m => m ()
+stop = undefined
 
 render :: GameState -> IO ()
 render gs = T.putStrLn [qms|state: {gs ^. xxx} {gs ^. yyy}: '{gs ^. txt}'|]
